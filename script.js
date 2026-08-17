3f17c43f1d67d2b78b3534b765ed1db6171fbefb
Felipe Cristino <felipebcristino@gmail.com>
2026-08-17 15:52:29 -0400

    terceiro

--- script.js
+++ script.js
@@ -1,5 +1,13 @@
 const body = document.body;
 const botao = document.getElementById("botao");
+const botao2 = document.getElementById("botao2");
+const arma1 = document.getElementById("arma1")
+const arma2 = document.getElementById("arma2")
+const arma3 = document.getElementById("arma3")
+const arma4 = document.getElementById("arma4")
+const arma5 = document.getElementById("arma5")
+const arma6 = document.getElementById("arma6")
+const personagemColide = [];
 
 class Personagem {
     constructor(px, py) {
@@ -9,10 +17,32 @@ class Personagem {
 }
 
 class Atirador extends Personagem {
-    constructor(px, py, range, velAtaque) {
+    constructor(px, py, cor) {
         super(px, py);
-        this.range = range;
-        this.velAtaque = velAtaque;
+        this.range = 20;
+        this.velAtaque = 5;
+        this.cor = cor;
+        this.tamx = 50;
+        this.tamy = 50;
+        this.id = Date.now() + "_" + Math.floor(Math.random() * 1000000000);
+        this.desenhar();
+        this.eu = document.getElementById(this.id);
+        personagemColide.push({
+            xMin:this.px - (this.tamx / 2),
+            xMax:this.px + (this.tamx / 2),
+            yMin:this.py - (this.tamy / 2),
+            yMax:this.py + (this.tamy / 2)
+        })
+    }
+
+    desenhar() {
+        const div = document.createElement("div");
+        div.setAttribute("id", this.id);
+        div.setAttribute("class", "atirador");
+        div.setAttribute("style", `left:${this.px - (this.tamx / 2)}px;
+        top:${this.py - (this.tamy / 2)}px;width:${this.tamx}px;
+        height:${this.tamy}px;background-color:${this.cor};`);
+        body.appendChild(div);
     }
 
     atirar() {
@@ -21,7 +51,7 @@ class Atirador extends Personagem {
 }
 
 const arrayCoresBaloes = ["red", "blue", "yellow",
-     "green", "white", "black"];
+    "green", "white", "black"];
 const arrayTamanhoBaloesx = [35, 38, 29, 28, 20, 22];
 const arrayTamanhoBaloesy = [42, 55, 38, 35, 29, 31];
 const velBaloes = [1.5, 1.4, 1.3, 1.2, 1.1, 1];
@@ -53,32 +83,32 @@ class Baloes {
     }
 
     controle_bordas() {
-        if(this.py > 36 && this.py < 56 && this.px > 350 && this.px < 360) {
+        if (this.py > 36 && this.py < 56 && this.px > 350 && this.px < 360) {
             this.dirx = 0;
             this.diry = 1;
         }
 
-        if(this.px > 340 && this.px < 360 && this.py > 223 && this.py < 230) {
+        if (this.px > 340 && this.px < 360 && this.py > 223 && this.py < 230) {
             this.dirx = 1;
             this.diry = 0;
         }
 
-        if(this.py > 210 && this.py < 230 && this.px > 660 && this.px < 670) {
+        if (this.py > 210 && this.py < 230 && this.px > 660 && this.px < 670) {
             this.dirx = 0;
             this.diry = 1;
         }
 
-        if(this.px > 647 && this.px < 667 && this.py > 380 && this.py < 390) {
+        if (this.px > 647 && this.px < 667 && this.py > 380 && this.py < 390) {
             this.dirx = -1;
             this.diry = 0;
         }
 
-        if(this.py > 380 && this.py < 390 && this.px > 240 && this.px < 250) {
+        if (this.py > 380 && this.py < 390 && this.px > 240 && this.px < 250) {
             this.dirx = 0;
             this.diry = 1;
         }
 
-        if(this.px > 240 && this.px < 250 && this.py > 537 && this.py < 547) {
+        if (this.px > 240 && this.px < 250 && this.py > 537 && this.py < 547) {
             this.dirx = -1;
             this.diry = 0;
         }
@@ -90,11 +120,10 @@ class Baloes {
         this.py += this.diry * this.vel;
         this.eu.setAttribute("style", `left:${this.px}px;top:${this.py}px;
         width:${this.tamx}px;height:${this.tamy}px;background-color:${this.cor};`);
-            if(this.px > (window.innerWidth - 250) || this.py > window.innerHeight
-            || this.py < 0 || (this.px < 0 && this.py > 500)) {
-                this.remover()
-            }
-        //CODIGO ACIMA DUVIDOSO(this.py > 500)=================
+        if (this.px > (window.innerWidth - 250) || this.py > window.innerHeight
+            || this.py < 0 || (this.px < 0 - this.tamx && this.py > 500)) {
+            this.remover()
+        }
     }
 
     remover() {
@@ -105,6 +134,151 @@ class Baloes {
 
 let balao1 = null;
 
-botao.addEventListener("click", ()=>{
+botao.addEventListener("click", () => {
     balao1 = new Baloes(1);
 })
+
+const removeSelecionado = () => {
+    document.querySelectorAll(".selecionado").forEach((sel) => {
+        sel.classList.remove("selecionado");
+    })
+}
+
+function selecionarBotao(botao, cor) {
+    const jaSelecionado = botao.classList.contains("selecionado");
+
+    // Remove a seleção de todos
+    document.querySelectorAll(".selecionado").forEach((elemento) => {
+        elemento.classList.remove("selecionado");
+    });
+
+    // Se ele NÃO estava selecionado, seleciona
+    if (!jaSelecionado) {
+        botao.classList.add("selecionado");
+    }
+
+    if (botao.classList.contains("selecionado")) {
+        document.addEventListener("click", (event)=>{
+            detectarClique(cor, event)
+        })
+    } else {
+       document.removeEventListener("click", (event)=>{
+            detectarClique(cor, event)
+        })
+    }
+}
+
+arma1.addEventListener("click", () => selecionarBotao(arma1, "red"));
+arma2.addEventListener("click", () => selecionarBotao(arma2, "blue"));
+arma3.addEventListener("click", () => selecionarBotao(arma3, "brown"));
+arma4.addEventListener("click", () => selecionarBotao(arma4, "yellow"));
+arma5.addEventListener("click", () => selecionarBotao(arma5, "green"));
+arma6.addEventListener("click", () => selecionarBotao(arma6, "orange"));
+
+const detectarClique = (cor, event) => {
+    const x = event.clientX;
+    const y = event.clientY;
+
+    if (!colidiu(x, y)) {
+        botao2.innerHTML = "X: " + x + " Y: " + y;
+        console.log(personagemColide);
+        const atirador1 = new Atirador(x, y, cor);
+    }
+};
+
+const obstaculo1 = {
+    xMin: 0,
+    xMax: 396,
+    yMin: 43,
+    yMax: 90
+}
+
+const obstaculo2 = {
+    xMin: 345,
+    xMax: 396,
+    yMin: 43,
+    yMax: 270
+}
+
+const obstaculo3 = {
+    xMin: 345,
+    xMax: 396,
+    yMin: 220,
+    yMax: 272
+}
+
+const obstaculo4 = {
+    xMin: 345,
+    xMax: 706,
+    yMin: 220,
+    yMax: 272
+}
+
+const obstaculo5 = {
+    xMin: 654,
+    xMax: 706,
+    yMin: 220,
+    yMax: 427
+}
+
+const obstaculo6 = {
+    xMin: 240,
+    xMax: 291,
+    yMin: 378,
+    yMax: 582
+}
+
+const obstaculo7 = {
+    xMin: 0,
+    xMax: 291,
+    yMin: 533,
+    yMax: 582
+}
+
+const obstaculo8 = {
+    xMin: 1122,
+    xMax: 1395,
+    yMin: 0,
+    yMax: 700
+}
+
+const obstaculos = [
+    obstaculo1,
+    obstaculo2,
+    obstaculo3,
+    obstaculo4,
+    obstaculo5,
+    obstaculo6,
+    obstaculo7,
+    obstaculo8
+];
+
+const colidiu = (x, y) => {
+    for (let i = 0; i < obstaculos.length; i++) {
+        const obstaculo = obstaculos[i];
+
+        if (
+            x >= obstaculo.xMin &&
+            x <= obstaculo.xMax &&
+            y >= obstaculo.yMin &&
+            y <= obstaculo.yMax
+        ) {
+            return true;
+        }
+    }
+
+    for (let i = 0; i < personagemColide.length; i++) {
+        const obstaculo = personagemColide[i];
+
+        if (
+            x >= obstaculo.xMin &&
+            x <= obstaculo.xMax &&
+            y >= obstaculo.yMin &&
+            y <= obstaculo.yMax
+        ) {
+            return true;
+        }
+    }
+
+    return false;
+};
